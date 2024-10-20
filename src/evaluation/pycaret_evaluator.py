1054b324df675736f4ba32db6820b7f6492e9e0f
mariemkallel16 <mariem.kallel@ensi-uma.tn>
2024-10-20 17:02:48 -0400

    Correcting the pycaret evaluator

--- src/evaluation/pycaret_evaluator.py
+++ src/evaluation/pycaret_evaluator.py
@@ -1,19 +1,19 @@
-import os
+import gc  # Garbage collector to free memory after each fold
 import json
+import os
 from time import strftime
-from typing import Dict, List, Optional, Union, Any
-from sklearn.model_selection import StratifiedKFold, KFold
-import pandas as pd
-from pycaret.classification import (
-    create_model, setup, pull, save_model, predict_model, tune_model
-)
+from typing import Any, Dict, List, Optional, Union
 
-import gc  # Garbage collector to liberate memory after each fold
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import StratifiedKFold, KFold
+
 import ray  # Import Ray
+from pycaret.classification import (create_model, predict_model, pull, save_model,
+                                    setup, tune_model)
 
 
 class PyCaretEvaluator:
-
     """
     Class to evaluate models using PyCaret, optimized for memory management.
     """
@@ -29,7 +29,6 @@ class PyCaretEvaluator:
             filepath (str): path for saving results.
             columns (Optional[List[str]]): optional list of column names.
         """
-
         self.dataset = dataset
         self.target = target
         self.experiment_name = experiment_name if experiment_name else f"experiment_{strftime('%Y%m%d-%H%M%S')}"
@@ -50,14 +49,43 @@ class PyCaretEvaluator:
         with open(os.path.join(self.filepath, filename), 'w', encoding='utf-8') as f:
             json.dump(results, f, indent=4)
 
-    @ray.remote(memory=8e9)  # Mark this function to be executed in parallel by Ray
-    def run_fold(self, train_index, test_index, fold_num, train_size, fold, fold_strategy, session_id, model, optimize, custom_grid, search_algorithm, fixed_params):
+    @ray.remote(memory=6e9)  # Mark this function to be executed in parallel by Ray
+    def run_fold(self, 
+                 train_index: np.ndarray, 
+                 test_index: np.ndarray, 
+                 fold_num: int, 
+                 train_size: float, 
+                 fold: int, 
+                 fold_strategy: str, 
+                 session_id: int, 
+                 model: str, 
+                 optimize: Union[str, List[str]], 
+                 custom_grid: Optional[Dict[str, List[Any]]], 
+                 search_algorithm: str, 
+                 fixed_params: Dict[str, Any]) -> Dict[str, Any]:
         """
         Run a single fold in parallel using Ray.
+
+        Args:
+            train_index (np.ndarray): Array of training indices for the fold.
+            test_index (np.ndarray): Array of testing indices for the fold.
+            fold_num (int): The current fold number.
+            train_size (float): Proportion of data to use for training within the fold.
+            fold (int): Number of folds for inner cross-validation.
+            fold_strategy (str): Strategy for inner cross-validation (e.g., 'kfold', 'stratifiedkfold').
+            session_id (int): Random seed for reproducibility.
+            model (str): Name of the model to be created (e.g., 'xgboost', 'lightgbm').
+            optimize (Union[str, List[str]]): Metric(s) to optimize during model tuning (e.g., 'AUC', 'Accuracy').
+            custom_grid (Optional[Dict[str, List[Any]]]): Custom hyperparameter grid for tuning the model.
+            search_algorithm (str): Hyperparameter search algorithm to use ('grid' or 'random').
+            fixed_params (Dict[str, Any]): Dictionary of fixed parameters for model setup (e.g., seed, eval_metric).
+
+        Returns:
+            Dict[str, Any]: A dictionary containing training results, test predictions, and the best hyperparameters for the fold.
         """
         print(f"Outer fold {fold_num}")
 
-        # Extract the subsets (using NumPy array indexing)
+        # Extract the subsets using the given indices from StratifiedKFold
         train_data_x = self.dataset.x[train_index]
         train_data_y = self.dataset.y[train_index]
         test_data_x = self.dataset.x[test_index]
@@ -69,16 +97,18 @@ class PyCaretEvaluator:
         test_df = pd.DataFrame(test_data_x, columns=self.columns)
         test_df[self.target] = test_data_y
 
-        # Configure PyCaret for the current fold with fixed parameters
+        print(f"Train indices: {train_index}")
+        print(f"Test indices: {test_index}")
+
+        # Configure PyCaret for the current fold
         exp = setup(data=train_df,
                     target=self.target,
                     train_size=train_size,
                     fold=fold,
-                    fold_strategy=fold_strategy,
-                    session_id=fixed_params['seed'],  # Using the seed for reproducibility
+                    fold_strategy=fold_strategy,  # Directly using StratifiedKFold internally if needed
+                    session_id=fixed_params['seed'],
                     verbose=False,
-                    n_jobs=1     
-            )
+                    n_jobs=1)
 
         print(f"Configuring PyCaret for outer fold {fold_num}")
 
@@ -87,17 +117,15 @@ class PyCaretEvaluator:
 
         if custom_grid:
             print(f"Tuning hyperparameters for model {model} with custom grid using {search_algorithm} search")
-            best_model = tune_model(
-                best_model,
-                custom_grid=custom_grid,
-                fold=fold,
-                optimize=optimize,
-                search_algorithm=search_algorithm,
-                verbose=False
-            )
+            best_model = tune_model(best_model, custom_grid=custom_grid, fold=fold, optimize=optimize, search_algorithm=search_algorithm, verbose=False)
+
+            # Extract the best hyperparameters after tuning
+            best_hyperparams = best_model.get_params()
+        else:
+            best_hyperparams = best_model.get_params()  # Default parameters if no tuning
 
         # Get the results and predictions
-        model_results = pull()  # Pulls the results after create_model or tune_model
+        model_results = pull()  # Pull the results after create_model or tune_model
         save_model(best_model, os.path.join(self.filepath, f"best_model_fold_{fold_num}"))
         test_predictions = predict_model(best_model, data=test_df)
 
@@ -105,7 +133,8 @@ class PyCaretEvaluator:
         split_result = {
             'fold': fold_num,
             'train_results': model_results.to_dict(),
-            'test_predictions': test_predictions.to_dict()
+            'test_predictions': test_predictions.to_dict(),
+            'best_hyperparams': best_hyperparams  # Save best hyperparameters for this fold
         }
 
         # Clean up memory after each fold
@@ -114,17 +143,17 @@ class PyCaretEvaluator:
 
         return split_result
 
-    def run_experiment(self,
-                       train_size: float = 0.8,
-                       fold: int = 5,
-                       fold_strategy: str = 'kfold',
-                       outer_fold: int = 5,
+    def run_experiment(self, 
+                       train_size: float = 0.8, 
+                       fold: int = 5, 
+                       fold_strategy: str = 'kfold', 
+                       outer_fold: int = 5, 
                        outer_strategy: str = 'stratifiedkfold',
-                       session_id: int = 42,
-                       model: Optional[str] = 'xgboost',
-                       optimize: Union[str, List[str]] = 'AUC',
+                       session_id: int = 42, 
+                       model: Optional[str] = 'xgboost', 
+                       optimize: Union[str, List[str]] = 'AUC', 
                        custom_grid: Optional[Dict[str, List[Any]]] = None,
-                       search_algorithm: str = 'grid',
+                       search_algorithm: str = 'grid', 
                        fixed_params: Dict[str, Any] = None) -> None:
         """
         Executes the complete experiment including external cross-validation, training, and model optimization.
@@ -142,14 +171,8 @@ class PyCaretEvaluator:
             search_algorithm (str): Algorithm to use for hyperparameter tuning ('grid' or 'random').
             fixed_params (Dict[str, Any]): Fixed parameters such as seed and eval_metric.
         """
-
-        # Use default fixed_params if none are provided
         if fixed_params is None:
-            fixed_params = {
-                'seed': 42,
-                'eval_metric': 'logloss',
-                'verbosity': 0
-            }
+            fixed_params = {'seed': 42, 'eval_metric': 'logloss', 'verbosity': 0}
 
         # Define the outer cross-validation strategy
         if outer_strategy == 'stratifiedkfold':
@@ -159,53 +182,47 @@ class PyCaretEvaluator:
         else:
             raise ValueError(f"Unknown outer_strategy: {outer_strategy}")
         
-        # Initialize Ray
-        ray.init(ignore_reinit_error=True, num_cpus=128)
-
+        ray.init(ignore_reinit_error=True, num_cpus=os.cpu_count())
         ray_tasks = []  # List to store Ray tasks
 
-        # External cross-validation loop
         for i, (train_index, test_index) in enumerate(outer_cv.split(self.dataset.x, self.dataset.y)):
-            # Launch each fold as a Ray task
-            ray_task = self.run_fold.remote(self, train_index, test_index, i + 1, train_size, fold, fold_strategy, session_id, model, optimize, custom_grid, search_algorithm, fixed_params)
+            ray_task = self.run_fold.remote(self, train_index, test_index, i + 1, train_size, fold, fold_strategy, session_id, model, 
+                                            optimize, custom_grid, search_algorithm, fixed_params)
             ray_tasks.append(ray_task)
 
-        # Collect all results from Ray tasks
         results = ray.get(ray_tasks)
-
-        # Save results in a JSON file
         self.save_results(results, f"{self.experiment_name}_results.json")
 
         # Collect and compute final metrics after training
         fold_metrics_list = []
+        best_hyperparams_list = []
 
         for result in results:
             train_results = result.get('train_results', {})
-
+            best_hyperparams = result.get('best_hyperparams', {})
             if isinstance(train_results, dict):
-                # Convert the results for each fold into a DataFrame
                 metrics_df = pd.DataFrame(train_results, index=[0])
                 fold_metrics_list.append(metrics_df)
+            if best_hyperparams:
+                best_hyperparams_list.append(best_hyperparams)
 
         if fold_metrics_list:
-            # Combine the DataFrames from all folds
             all_fold_metrics = pd.concat(fold_metrics_list, ignore_index=True)
-
-            # Calculate the mean and standard deviation for each metric across all folds
             final_metrics_mean = all_fold_metrics.mean()
             final_metrics_std = all_fold_metrics.std()
 
-            # Create a summary table of the final metrics
-            metrics_table = pd.DataFrame({
-                'Metric': final_metrics_mean.index,
-                'Mean': final_metrics_mean.values,
-                'Std Dev': final_metrics_std.values
-            })
-
+            metrics_table = pd.DataFrame({'Metric': final_metrics_mean.index, 'Mean': final_metrics_mean.values, 
+                                          'Std Dev': final_metrics_std.values})
             print("Final metrics table:")
             print(metrics_table)
 
-            # Save the final metrics table to a CSV file
             metrics_table.to_csv(os.path.join(self.filepath, f"{self.experiment_name}_final_metrics.csv"), index=False)
-            
-            ray.shutdown()
+
+        # Selecting the best hyperparameters based on the fold results
+        if best_hyperparams_list:
+            # You can either calculate the most frequent hyperparameters or take an average depending on the nature
+            best_hyperparams_df = pd.DataFrame(best_hyperparams_list)
+            most_common_hyperparams = best_hyperparams_df.mode().iloc[0]  # Most frequent hyperparameters across folds
+            print(f"Best hyperparameters across all folds: {most_common_hyperparams}")
+
+        ray.shutdown()
